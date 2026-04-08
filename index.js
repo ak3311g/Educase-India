ce5b9d8e4e035e4eee21805d08772335e34df614
Rajiv Kumar <ak3311g@gmail.com>
2026-04-08 19:34:13 +0530

    package json update

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(
+/* app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   }),
-);
+); */
 
 app.use(express.json())
 
